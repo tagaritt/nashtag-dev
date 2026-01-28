14be6059e977b535e7f99c77b7d199d17b37f676
tagaritt <47823969+tagaritt@users.noreply.github.com>
2026-01-28 10:17:46 -0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,9 +4,9 @@ const page = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
         <div className="space-y-8 max-w-10xl">
-            <h1 className='text-4xl font-bold text-left ml-10 mt-20'>About Me</h1>
+            <h1 className='text-4xl font-bold text-left mx-100 mt-10'>About Me</h1>
 
-            <p className='text-lg text-center mt-6 mx-40 text-gray-400'>
+            <p className='text-lg text-left mt-6 mx-100 text-gray-400'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacus nisl, rutrum sed interdum sit amet, consectetur sed eros. Praesent sed sollicitudin dui. In posuere diam turpis, vitae vulputate eros eleifend a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum volutpat sit amet odio et iaculis. Aliquam auctor lacus ut quam efficitur maximus. Proin placerat semper luctus. Integer finibus sem sed turpis porta scelerisque. Phasellus nec leo ligula. Pellentesque blandit aliquet dolor, varius placerat nulla. Nullam malesuada, odio condimentum vestibulum vestibulum, est risus congue velit, eget ultricies lectus nibh sed libero. Aenean auctor, mauris quis ultrices varius, urna odio dapibus tortor, nec imperdiet elit lectus id lectus. Pellentesque tincidunt maximus facilisis. Class aptent taciti.
             </p>
         </div>
