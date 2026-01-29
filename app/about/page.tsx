d4e8ebab51852ecb0816e196cedbbe531b0a9083
tagaritt <47823969+tagaritt@users.noreply.github.com>
2026-01-29 13:53:47 -0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,14 +3,22 @@ import React from 'react'
 const page = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
-        <div className="space-y-8 max-w-10xl">
-            <h1 className='text-4xl font-bold text-left mx-100 mt-10'>About Me</h1>
+        <div className="space-y-8 max-w-8xl">
+            <h1 className='text-4xl font-bold text-left mx-105 mt-10'>About Me</h1>
 
-            <p className='text-lg text-left mt-6 mx-100 text-gray-400'>
+            <p className='text-lg text-left mt-6 mx-105 text-gray-400'>
                 I’m a software engineer with a knack for web design and data engineering, leading to me building eye-pleasing, storytelling databases and interfaces. I find joy in building things that help and that users enjoy interacting with, placing myself in a melting pot of design and engineering. 
             </p>
 
-            <p className='text-lg text-left mt-6 mx-100 text-gray-400'>
+            <p className='text-lg text-left mt-6 mx-105 text-gray-400'>
+                I come with experience from multiple industries including automotive, pharmaceuticals, healthcare, and high tech having worked with companies such as <b className="text-white"><a href="https://www.honda.com/operations">Honda</a></b>, <b className="text-white"><a href="https://www.regeneron.com/">Regeneron</a></b>, <b className="text-white">Intel</b>, and others. Outside of what I do professionally, I continue to develop passion projects in order to grow as a developer and hopefully lead the next generation of developers.
+            </p>
+
+            <p className='text-lg text-left mt-6 mx-105 text-gray-400'>
+                Right now, I work as an Advanced Application Engineering Analyst for Accenture, where I've worked on various teams to successfully bring solutions to fruition. While working here, I've lead teams in building API endpoints and pilot program demos, acted as an SME for testing purposes, and built features for customer-facing applications.
+            </p>
+
+            <p className='text-lg text-left mt-6 mx-105 text-gray-400'>
                 When I'm not developing, I tend to play video games, create through photography/videography, or plot on a happy life with my girlfriend. 
             </p>
         </div>
