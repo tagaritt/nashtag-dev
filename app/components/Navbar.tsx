de244cb84fdd99a2f4d945d09a46dbe0ac06f4f2
tagaritt <47823969+tagaritt@users.noreply.github.com>
2026-02-15 23:34:06 -0500

    Navbar optimized

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className='fixed pt-4 w-full flex items-center justify-around py-1 px-40 backdrop-blur-sm bg-black/30 z-50 border-b-1 border-gray-950'>
+    <nav className='fixed w-full flex items-center justify-center py-1 px-40 backdrop-blur-sm z-50 border-b-1 border-b-neutral-950/10'>
         {/* This is code for my logo.*/}
         <Link href="/" className="transition duration-300 hover:scale-105">
             <Image src="/nashtag-logo.png" alt="NashTag logo" width={ 45 } height={ 45 }/>
         </Link>
 
         {/* This is where all of the other links in the navbar will go. */}
-        <ul className="flex gap-14 text-xl">
+        <ul className="flex gap-14 text-xl ml-120 p-5 itmes-center justify-center">
             <Link href="/" className="text-white font-semibold hover:animate-pulse transition-colors">Home</Link> 
             <Link href="/about" className="text-white font-semibold hover:text-white transition-colors">About</Link>
             <Link href="/experience" className="text-white font-semibold hover:text-white transition-colors">Experience</Link>
