b6257e54141eabdd2ed9ca1db1c25935043e16a9
tagaritt <47823969+tagaritt@users.noreply.github.com>
2026-02-22 13:00:43 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={montserrat.className}>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <body className={`${montserrat.variable} antialiased`}>
 
         <Navbar></Navbar>
